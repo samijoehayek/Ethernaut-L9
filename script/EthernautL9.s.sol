33fdbaf0d0d5aabf1ec1f85a5905bc8ea466adde
SamijoeHayek <samijoehayek1@gmail.com>
2024-04-22 23:25:45 +0300

    first commit

--- script/EthernautL9.s.sol
+++ script/EthernautL9.s.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
-
+contract EthernautL9Script is Script {
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        vm.stopBroadcast();
     }
 }
